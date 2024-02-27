ca83001b1fbe97f649a54c6a58d8f7199c048b71
CodeNinjaUzb <codeninjauzb@gmail.com>
2024-02-28 02:50:38 +0500

    four commit

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
+import Helmet from '../components/Helmet/Helmet';
 
 import '../styles/contact.css'
 
 function Contact() {
       return (
-            <div className='contact d-flex align-items-center justify-content-center'>
-                  <div className="info text-center">
-                        <h2>Website developed by Code Ninja!</h2>
-                        <h4>Phone number : +998992293737</h4>
-                        <div className="telegram d-flex align-items-center justify-content-center gap-1">
-                              <i className='ri-telegram-fill text-info'></i>
-                              <a href='https://t.me/code_ninja_dev' className='text-decoration-none text-black'>@code_ninja_dev</a>
+            <Helmet title="Contact">
+                  <div className='contact d-flex align-items-center justify-content-center'>
+                        <div className="info text-center">
+                              <h2>Website developed by Code Ninja!</h2>
+                              <h4>Phone number : +998992293737</h4>
+                              <div className="telegram d-flex align-items-center justify-content-center gap-1">
+                                    <i className='ri-telegram-fill text-info'></i>
+                                    <a href='https://t.me/code_ninja_dev' className='text-decoration-none text-black'>@code_ninja_dev</a>
+                              </div>
                         </div>
                   </div>
-            </div>
+            </Helmet>
       );
 }
 
